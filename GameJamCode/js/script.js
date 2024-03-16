b3463fdc5f458cce12acad01712b341680fecbde
mayamarshel <144174991+mayamarshel@users.noreply.github.com>
2024-03-16 13:24:37 -0600

    Set up Phaser and began practice project
    - linked library
    - started pippin practice thing

---
+++ GameJamCode/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+  },
+  scene: []
+};
+
+// Here we actually create the game using this configuration!
+let game = new Phaser.Game(config);
